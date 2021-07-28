70ae6953c2631f4284c0ff92e610af717bd3076a
Rick Carlino <rick.carlino@gmail.com>
2021-07-28 17:06:45 -0500

    Test fixes for CSVM variable updates

--- farmbot_core/test/farmbot_celery_script/integration_test.exs
+++ farmbot_core/test/farmbot_celery_script/integration_test.exs
@@ -1,14 +1,18 @@
 # This is a "kitchen sink" of sorts.
 defmodule FarmbotCeleryScript.IntegrationTest do
   use ExUnit.Case
+  use Mimic
+
   alias FarmbotCeleryScript.Compiler
   alias FarmbotCeleryScript.AST
   alias FarmbotCeleryScript.Compiler.Scope
 
+  setup :verify_on_exit!
+
   @fixtures [
     "test/fixtures/execute.json",
     "fixture/inner_sequence.json",
-    "fixture/master_sequence.json",
+    "fixture/every_sequence.json",
     "fixture/outer_sequence.json",
     "fixture/paramater_sequence.json",
     "fixture/point_group_sequence.json",
@@ -20,10 +24,15 @@ defmodule FarmbotCeleryScript.IntegrationTest do
   ]
 
   test "all the fixtures (should not crash!)" do
+    expect(FarmbotCeleryScript.SysCalls, :get_sequence, 7, fn id ->
+      compile_celery_file("fixture/inner_sequence.json")
+    end)
+
     Enum.map(@fixtures, &compile_celery_file/1)
   end
 
   def compile_celery_file(json_path) do
+    IO.puts("==== BEGIN " <> json_path)
     json_path
     |> File.read!()
     |> Jason.decode!()
